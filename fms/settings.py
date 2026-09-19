4c1464cca2cd538f6ccd4e013acdb27e802bf557
Sourav Das <souravdas007123@gmail.com>
2026-09-19 10:54:23 +0530

    update

--- fms/settings.py
+++ fms/settings.py
@@ -88,7 +88,7 @@ TIME_ZONE = "Asia/Kolkata"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
