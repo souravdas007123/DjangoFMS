12a986a9de6f339002c822f69fba06ec12c1b033
Sourav Das <souravdas007123@gmail.com>
2026-09-19 21:34:34 +0530

    update

--- fms/urls.py
+++ fms/urls.py
@@ -5,10 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", include("apps.dashboard.urls")),
     path("admin/", admin.site.urls),
-    path("accounts/login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
-    path("accounts/logout/", auth_views.LogoutView.as_view(next_page="/accounts/login/"), name="logout"),
 ]
 
 if settings.DEBUG:
